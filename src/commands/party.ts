4eddec9624611025a662470f34e39d3925bebb24
Justin Beckwith <justin.beckwith@gmail.com>
2025-01-11 13:57:16 -0800

    add party mode (#4)

---
+++ src/commands/party.ts
@@ -0,0 +1,38 @@
+import {
+	type ChatInputCommandInteraction,
+	InteractionContextType,
+	SlashCommandBuilder,
+} from 'discord.js';
+import * as config from '../config.js';
+import { api, realtime } from '../twinkly.js';
+
+export const data = new SlashCommandBuilder()
+	.setName('party')
+	.setDescription('Puts the lights into party mode 🎊')
+	.setContexts([
+		InteractionContextType.BotDM,
+		InteractionContextType.Guild,
+		InteractionContextType.PrivateChannel,
+	]);
+
+export async function execute(interaction: ChatInputCommandInteraction) {
+	const n = await interaction.deferReply();
+	await api.setLEDOperationMode({ mode: api.LEDOperationMode.RT });
+	const token = api.getToken();
+
+	for (let i = 0; i < 100; i++) {
+		const nodes = [];
+		for (let i = 0; i < 190; i++) {
+			nodes.push({
+				r: Math.floor(Math.random() * 255),
+				g: Math.floor(Math.random() * 255),
+				b: Math.floor(Math.random() * 255),
+			});
+		}
+
+		await realtime.sendFrame(config.twinklyIP, token, nodes);
+		await new Promise((resolve) => setTimeout(resolve, 50));
+	}
+
+	await interaction.editReply('Party mode complete! 🎊');
+}
--- src/twinkly.ts
+++ src/twinkly.ts
@@ -1,6 +1,6 @@
-import { api } from '@twinklyjs/twinkly';
+import { api, realtime } from '@twinklyjs/twinkly';
 import * as config from './config.js';
 
 api.init(config.twinklyIP);
 
-export { api };
+export { api, realtime };
